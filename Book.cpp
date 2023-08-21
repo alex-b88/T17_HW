ae37ad73d6032f8ce424fc5630c0bb454198efc7
Oleksandr Bak <alex88.b@icloud.com>
2023-08-21 14:17:41 +0300

    Created Book.cpp

--- Book.cpp
+++ Book.cpp
@@ -1,2 +1,29 @@
 
 #include "Book.h"
+
+Book::Book() {
+    summary = "Резюме не заполнено";
+}
+
+Book::Book(string name, string author, Date publish_year, string summary)
+: Publication(name,author,publish_year)
+{
+    setSummary(summary);
+}
+
+Book::~Book() {
+
+}
+
+void Book::setSummary(const string &summary) {
+    Book::summary=summary;
+}
+
+const string &Book::getSummary() const {
+    return summary;
+}
+
+void Book::show() const {
+    Publication::show();
+    cout <<"Резюме: " << summary << endl;
+}
